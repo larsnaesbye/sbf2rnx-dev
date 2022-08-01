2714434c57d141c378e50b97bd03a2097b3c833e
Lars Næsbye Christensen <lars@naesbye.dk>
2022-08-01 14:57:36 +0200

    A little more structure

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # sbf2rnx-dev
 
-See if I can convert SBF (Septentrio) files to RINEX
+See if I can convert SBF (Septentrio) binary files to RINEX.
 
-The format, though proprietary, is openly described in Septentrio documents. Others have made parsers for it, e.g. here: https://github.com/asvol/gnss.net/blob/main/src/Asv.Gnss/Parsers/SBF/SbfBinaryParser.cs
+The format, though proprietary, is openly described in Septentrio documents.
+
+Others have made parsers for it, e.g. here: https://github.com/asvol/gnss.net/blob/main/src/Asv.Gnss/Parsers/SBF/SbfBinaryParser.cs
+
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::load_yaml;
 use clap::App;
+use rinex::Rinex;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -10,11 +11,27 @@ pub enum Error {
 
 pub fn main() -> Result<(), Error> {
     let yaml = load_yaml!("cli.yml");
-    let app = App::from_yaml(yaml);
+    let app = App::from_yaml(yaml); // TODO: replace deprecated YAML call with arg-builder
     let matches = app.get_matches();
+
+    let filepaths: Option<Vec<&str>> = match matches.is_present("filepath") {
+        true => {
+            Some(matches.value_of("filepath")
+                .unwrap()
+                .split(",")
+                .collect())
+        }
+        false => None,
+    };
+
+    let mut rinexrec = sbf2rnxrec(filepaths);
+    write_rnx_file();
     return Ok(());
 }
 
-fn sbfrec2rnxrec() {
-    println!("nada yet")
+fn sbf2rnxrec(filepaths: Option<Vec<&str>>) -> Rinex {
+// TODO: build RINEX records and output them as files
+    return Rinex::default();
 }
+
+fn write_rnx_file() {}
