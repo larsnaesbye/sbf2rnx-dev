e9a057be43a9603a21a5d217e39483ce34c8aec1
Lars Næsbye Christensen <lars@naesbye.dk>
2022-08-02 14:36:53 +0200

    Start the SBF struct building

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 //! This application converts Septentrio binary files
 //! into receiver independent (RINEX) files.  
 
+mod sbf;
+
 use clap::load_yaml;
 use clap::App;
 use rinex::Rinex;
@@ -13,23 +15,25 @@ pub enum Error {
 }
 
 pub fn main() -> Result<(), Error> {
-    let yaml = load_yaml!("cli.yml");
+    let yaml = load_yaml!("arg.yaml");
     let app = App::from_yaml(yaml); // TODO: replace deprecated YAML call with arg-builder
     let matches = app.get_matches();
 
-    let filepaths: Option<Vec<&str>> = match matches.is_present("filepath") {
+    let filepath: Option<Vec<&str>> = match matches.is_present("filepath") {
         true => Some(matches.value_of("filepath").unwrap().split(",").collect()),
         false => None,
     };
 
-    let mut rinexrec = sbf2rnxrec(filepaths);
+    let rinexrec = sbf2rnxrec(filepath);
     write_rnx_file(rinexrec);
     return Ok(());
 }
 
-fn sbf2rnxrec(filepaths: Option<Vec<&str>>) -> Rinex {
+fn sbf2rnxrec(_filepath: Option<Vec<&str>>) -> Rinex {
     // TODO: build RINEX records and output them as files
     return Rinex::default();
 }
 
-fn write_rnx_file(rinexrec: Rinex) {}
+fn write_rnx_file(rinexrec: Rinex) {
+    rinexrec.to_file("").expect("TODO: panic message");
+}
--- src/sbf.rs
+++ src/sbf.rs
@@ -1,2 +1,21 @@
+//! The extracted information from an SBF file, arranged
+
 #[derive(Clone, Debug)]
-struct SbfRecord {}
+pub struct SbfBlockHeader {
+    sync: [char; 2], // always set to 0x24, 0x40 = "$@"
+    crc: u16,
+    id: u16,
+    length: u16,
+}
+
+#[derive(Clone, Debug)]
+pub struct SbfBlockTimestamp {
+    tow: u32,
+    wnc: u16,
+}
+
+#[derive(Clone, Debug)]
+pub struct SbfBlock {
+    header: SbfBlockHeader,
+    timestamp: SbfBlockTimestamp,
+}
