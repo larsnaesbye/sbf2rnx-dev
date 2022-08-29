ff00d6e3663ddd976de0b3aa80d534fa6d3f9a7f
Lars Næsbye Christensen <lars@naesbye.dk>
2022-08-29 23:31:55 +0200

    Finish message list

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # sbf2rnx-dev
 
-A fun little Rust side project to see if I can convert SBF (Septentrio) binary files to RINEX.
+A fun little side project to see if I can convert SBF (Septentrio) binary files to RINEX using Rust.
 
 The format, though proprietary, is openly described in the Septentrio reference guide, e.g. page 244 on https://www.septentrio.com/system/files/support/asterx_sb_firmware_v4.8.4_reference_guide.pdf
 
-Others have made parsers for it, e.g. here: https://github.com/asvol/gnss.net/blob/main/src/Asv.Gnss/Parsers/SBF/SbfBinaryParser.cs
+Others have made parsers for it, e.g. here: https://github.com/asvol/gnss.net/blob/main/src/Asv.Gnss/Parsers/SBF/SbfBinaryParser.cs or https://raw.githubusercontent.com/tomojitakasu/RTKLIB/master/src/rcv/septentrio.c 
 
--- src/sbf.rs
+++ src/sbf.rs
@@ -111,5 +111,80 @@ enum SBASL1DecodedMessageBlocks {
 enum PositionVelocityAndTimeBlocks {
     PVICartesian = 4006,
     PVIGeodetic = 4007,
-    // Fill in more from the PDF
+    PosCovCartesian = 5905,
+    PosCovGeodetic = 5906,
+    VelCovCartesian = 5907,
+    VelCovGeodetic = 5908,
+    DOP = 4001,
+    PosCart = 4044,
+    PosLocal = 4052,
+    PosProjected = 4094,
+    PVISatCartesian = 4008,
+    PVIResiduals = 4009,
+    RAIMStatistics = 4011,
+    GEOCorrections = 5935,
+    BaseVectorCart = 4043,
+    BaseVectorGeod = 4028,
+    FVISupport = 4076,
+    FVISupportA = 4079,
+    EndOfPVI = 5921,
+}
+
+enum ReceiverTimeBlocks {
+    ReceiverTime = 5914,
+    xPPSOffset = 5911,
+}
+
+enum ExternalEventBlocks {
+    ExtEvent = 5924,
+    ExtEventPVICartesian = 4037,
+    ExtEventPVIGeodetic = 4038,
+    ExtEventBaseVectGeod = 4217,
+}
+
+enum DifferentialCorrectionBlocks {
+    DiffCorrIn = 5919,
+    BaseStation = 5949,
+    RICMDatum = 4049,
+}
+
+enum LBandDemodulatorBlocks {
+    LBandTrackerStatus = 4201,
+    LBAS1DecoderStatus = 4202,
+    LBAS1Messages = 4203,
+    LBandBeams = 4204,
+}
+enum StatusBlocks {
+    ChannelStatus = 4013,
+    ReceiverStatus = 4014,
+    SatVisibility = 4012,
+    InputLink = 4090,
+    OutputLink = 4091,
+    NTRIPClientStatus = 4053,
+    NTRIPServerStatus = 4122,
+    IPStatus = 4058,
+    WiFiAPStatus = 4054,
+    WiFiClientStatus = 4096,
+    DynDNSStatus = 4105,
+    PowerStatus = 4101,
+    QualityInd = 4082,
+    DiskStatus = 4059,
+    LogStatus = 4102,
+    RFStatus = 4092,
+    P2PPStatus = 4238,
+    CosmosStatus = 4243,
+}
+
+enum MiscellaneousBlocks {
+    ReceiverSetup = 5902,
+    RxComponents = 4084,
+    RxMessage = 4103,
+    Commands = 4015,
+    Comment = 5936,
+    BBSamples = 4040,
+    ASCIIIn = 4075,
+}
+
+enum AdvancedBlocks {
+    SystemInfo = 6000,
 }
