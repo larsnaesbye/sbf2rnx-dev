59cd2dc88d1bccf9cc1e123be4da3aae36274ae7
Lars Næsbye Christensen <lars@naesbye.dk>
2022-09-01 15:21:20 +0200

    Add placeholder test

---
+++ tests/tests.rs
@@ -0,0 +1,9 @@
+//! Here we define unit tests and other tests to be run
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() { // TODO: replace with real tests
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
