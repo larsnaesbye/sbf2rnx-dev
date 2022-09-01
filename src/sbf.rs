ae1fdfa655a0fb4d93a404bd5166e520326ab580
Lars Næsbye Christensen <lars@naesbye.dk>
2022-09-01 11:18:39 +0200

    Add placeholders for decoding functions

--- src/sbf.rs
+++ src/sbf.rs
@@ -20,22 +20,20 @@ pub struct SbfBlock {
     timestamp: SbfBlockTimestamp,
 }
 
-// ====== SBF BLOCKS ======
+// ====== SBF BLOCK DATA ======
 
-// --- Measurement Blocks ---
-enum MeasurementBlocks {
-    MeasEpoch = 4027,
-    MeasExtra = 4000,
-    Meas3Ranges = 4109,
-    Meas3CN0HiRes = 4110,
-    Meas3Doppler = 4111,
-    Meas3PP = 4112,
-    Meas3MP = 4113,
-    EndOfMeas = 5922,
-}
+enum SBFBlocks {
+    // --- Measurement Blocks ---
+    MeasEpoch = 4027,     // Measurement set of one epoch
+    MeasExtra = 4000,     // Additional info such as observable variance
+    Meas3Ranges = 4109,   // Code, phase and CN0 measurements
+    Meas3CN0HiRes = 4110, // Extension of Meas3Ranges containing fractional C/N0 values
+    Meas3Doppler = 4111,  // Extension of Meas3Ranges containing Doppler values
+    Meas3PP = 4112, // Extension of Meas3Ranges containing proprietary flags for data post-processing
+    Meas3MP = 4113, // Extension of Meas3Ranges containing multipath corrections applied by the receiver.
+    EndOfMeas = 5922, // Measurement epoch marker
 
-// --- Navigation Page Blocks
-enum NavigationPageBlocks {
+    // --- Navigation Page Blocks
     GPSRawCA = 4017,
     GPSRawL2C = 4018,
     GPSRawL5 = 4019,
@@ -57,41 +55,37 @@ enum NavigationPageBlocks {
     QZSRawL1C = 4227,
     QZSRawL1S = 4228,
     NAVICRaw = 4093,
-}
-enum GPSDecodedMessageBlocks {
+
+    // GPS Decoded Message blocks
     GPSNav = 5891,
     GPSAlm = 5892,
     GPSIon = 5893,
     GPSUtc = 5894,
-}
 
-enum GLONASSDecodedMessageBlocks {
+    // GLONASS Decoded Message blocks
     GLONav = 4004,
     GLOAlm = 4005,
     GLOTime = 4036,
-}
 
-enum GalileoDecodedMessageBlocks {
+    // Galileo Decoded Message blocks
     GALNav = 4002,
     GALAlm = 4003,
     GALIon = 4030,
     GALUtc = 4031,
     GALGstGps = 4032,
     GALSARRLM = 4034,
-}
 
-enum BeiDouDecodedMessageBlocks {
+    // BeiDou Decoded Message blocks
     BDSNav = 4081,
     BDSAlm = 4119,
     BDSIon = 4120,
     BDSUtc = 4121,
-}
-enum QZSSDecodedMessageBlocks {
+
+    // QZSS Decoded Message blocks
     QZSNav = 4095,
     QZSAlm = 4116,
-}
 
-enum SBASL1DecodedMessageBlocks {
+    // SBAS L1 Decoded Message blocks
     GEOMT00 = 5925,
     GEOPRNMask = 5926,
     GEOFastCorr = 5927,
@@ -106,9 +100,8 @@ enum SBASL1DecodedMessageBlocks {
     GEOIonoDelay = 5933,
     GEOServiceLevel = 5917,
     GEOClockEphcovMatrix = 5934,
-}
 
-enum PositionVelocityAndTimeBlocks {
+    // Position, Velocity and Time Blocks
     PVICartesian = 4006,
     PVIGeodetic = 4007,
     PosCovCartesian = 5905,
@@ -128,33 +121,29 @@ enum PositionVelocityAndTimeBlocks {
     FVISupport = 4076,
     FVISupportA = 4079,
     EndOfPVI = 5921,
-}
 
-enum ReceiverTimeBlocks {
+    // Receiver Time Blocks
     ReceiverTime = 5914,
     xPPSOffset = 5911,
-}
 
-enum ExternalEventBlocks {
+    // External Event Blocks
     ExtEvent = 5924,
     ExtEventPVICartesian = 4037,
     ExtEventPVIGeodetic = 4038,
     ExtEventBaseVectGeod = 4217,
-}
 
-enum DifferentialCorrectionBlocks {
+    // Differential Correction Blocks
     DiffCorrIn = 5919,
     BaseStation = 5949,
     RICMDatum = 4049,
-}
 
-enum LBandDemodulatorBlocks {
+    // L-Band Demodulator Blocks
     LBandTrackerStatus = 4201,
     LBAS1DecoderStatus = 4202,
     LBAS1Messages = 4203,
     LBandBeams = 4204,
-}
-enum StatusBlocks {
+
+    // Status Blocks
     ChannelStatus = 4013,
     ReceiverStatus = 4014,
     SatVisibility = 4012,
@@ -173,9 +162,8 @@ enum StatusBlocks {
     RFStatus = 4092,
     P2PPStatus = 4238,
     CosmosStatus = 4243,
-}
 
-enum MiscellaneousBlocks {
+    // Miscellaneous Blocks
     ReceiverSetup = 5902,
     RxComponents = 4084,
     RxMessage = 4103,
@@ -183,8 +171,69 @@ enum MiscellaneousBlocks {
     Comment = 5936,
     BBSamples = 4040,
     ASCIIIn = 4075,
+
+    // Advanced Blocks
+    SystemInfo = 6000,
 }
 
-enum AdvancedBlocks {
-    SystemInfo = 6000,
+// --- Decoding functions ---
+
+fn decode_measepoch(measepochdata: u8) -> u8 {
+    return measepochdata; // Placeholder
+}
+
+fn decode_gpsnav(gpsnavdata: u8) -> u8 {
+    return gpsnavdata; // Placeholder
+}
+
+fn decode_galnav(galnavdata: u8) -> u8 {
+    return galnavdata; // Placeholder
+}
+
+fn decode_glonav(glonavdata: u8) -> u8 {
+    return glonavdata; // Placeholder
+}
+
+fn decode_sbasnav(sbasnavdata: u8) -> u8 {
+    return sbasnavdata; // Placeholder
+}
+
+fn decode_gpsrawcanav(gpsrawcanavdata: u8) -> u8 {
+    return gpsrawcanavdata; // Placeholder
+}
+
+fn decode_georaw(georawdata: u8) -> u8 {
+    return georawdata; // Placeholder
+}
+
+fn decode_glorawcanav(glorawcanavdata: u8) -> u8 {
+    return glorawcanavdata; // Placeholder
+}
+
+fn decode_gpsion(gpsiondata: u8) -> u8 {
+    return gpsiondata; // Placeholder
+}
+
+fn decode_galion(galiondata: u8) -> u8 {
+    return galiondata; // Placeholder
+}
+
+fn decode_gpsutc(gpsutcdata: u8) -> u8 {
+    return gpsutcdata; // Placeholder
+}
+
+fn decode_gpsalm(gpsalmdata: u8) -> u8 {
+    return gpsalmdata; // Placeholder
+}
+
+fn decode_galutc(galutcdata: u8) -> u8 {
+    return galutcdata; // Placeholder
+}
+
+fn decode_galalm(galalmdata: u8) -> u8 {
+    return galalmdata; // Placeholder
+}
+
+fn decode_sbf(sbfdata: u8) -> u8 {
+    return sbfdata; // Placeholder
 }
