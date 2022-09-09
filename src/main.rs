0a05d8c0cdf9cc22f604d1db9706f400eb9255ba
Lars Næsbye Christensen <lars@naesbye.dk>
2022-09-09 12:07:22 +0200

    Move arguments to separate function for clarity

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,8 @@
 //! Specification is found on page 244 on https://www.septentrio.com/system/files/support/asterx_sb_firmware_v4.8.4_reference_guide.pdf
 mod sbf;
 
-use clap::AppSettings;
-use clap::Arg;
 use clap::Command;
+use clap::{Arg, ArgMatches};
 use rinex::Rinex;
 use thiserror::Error;
 
@@ -16,16 +15,7 @@ pub enum Error {
 }
 
 pub fn main() -> Result<(), Error> {
-    let matches = Command::new("sbf2rnx-dev")
-        .version("0.0.1")
-        .author("Lars Næsbye Christensen <lars@naesbye.dk>")
-        .about("Preliminary work for a Septentrio SBF to RINEX converter")
-        .arg(Arg::new("filepath").short('f').long("filepath"))
-        .arg(Arg::new("interval").short('i').long("interval"))
-        .arg(Arg::new("output").short('o').long("output"))
-        .arg_required_else_help(true)
-        .get_matches();
-
+    let matches = matches();
     let filepath: Option<Vec<&str>> = match matches.is_present("filepath") {
         true => Some(matches.value_of("filepath").unwrap().split(",").collect()),
         false => None,
@@ -34,6 +24,24 @@ pub fn main() -> Result<(), Error> {
     let rinexrec = sbf2rnxrec(filepath);
     write_rnx_file(rinexrec);
     return Ok(());
+}
+
+fn matches() -> ArgMatches {
+    let matches = Command::new("sbf2rnx-dev")
+        .version("0.0.1")
+        .author("Lars Næsbye Christensen <lars@naesbye.dk>")
+        .about("Preliminary work for a Septentrio SBF to RINEX converter")
+        .arg(
+            Arg::new("filepath")
+                .short('f')
+                .long("filepath")
+                .required(true),
+        )
+        .arg(Arg::new("interval").short('i').long("interval"))
+        .arg(Arg::new("output").short('o').long("output"))
+        .arg_required_else_help(true)
+        .get_matches();
+    return matches;
 }
 
 fn sbf2rnxrec(_filepath: Option<Vec<&str>>) -> Rinex {
