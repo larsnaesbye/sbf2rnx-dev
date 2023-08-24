5d305c5a34989043ddf75699adc7826c80c411f5
Lars Næsbye Christensen <lars@naesbye.dk>
2023-08-24 23:27:08 +0200

    Cleanup

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ The format, though proprietary, is openly described in the Septentrio reference 
 
 Others have made parsers for it, e.g. here: https://github.com/asvol/gnss.net/blob/main/src/Asv.Gnss/Parsers/SBF/SbfBinaryParser.cs or https://raw.githubusercontent.com/tomojitakasu/RTKLIB/master/src/rcv/septentrio.c 
 
+If it works, it might be turned into a plugin for the georust/rinex crate.
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@
 //! into receiver independent (RINEX) files.  
 //! Specification is found on page 244 on https://www.septentrio.com/system/files/support/asterx_sb_firmware_v4.8.4_reference_guide.pdf
 mod sbf;
-mod cli;
 
-use cli::matches;
 use rinex::Rinex;
 use std::path::Path;
 use thiserror::Error;
@@ -16,36 +14,36 @@ pub enum Error {
 }
 
 pub fn main() -> Result<(), Error> {
-    let matches = matches();
-    //let filepath: Option<Vec<&str>> = match matches.is_present("filepath") {
-    //    true => Some(matches.value_of("filepath").unwrap().split(",").collect()),
-    //    false => None,
-    //};
-    // get the file data
-    //let rinexrec = sbf2rnxrec(filepath.unwrap());
-    //write_rnx_file(rinexrec);
+    let filepath: &str = "../../tests/testdata/KMS3240s.22_";
+    let rinexrec = sbf2rnxrec(filepath);
+    write_rnx_file(rinexrec);
     return Ok(());
 }
 
-fn sbf2rnxrec(filepath: Vec<&str>) -> Rinex {
+fn sbf2rnxrec(filepath: &str) -> Rinex {
     //! Build RINEX records and output them as files
     // For now we read the entire file as bytes before conversion - this uses more memory!
-    //match std::fs::read(Path::new(&filepath.as_ptr())) {
-    //    Ok(bytes) => {
-    //        eprintln!("{}", bytes.len().to_string()) // For testing, just print the length of the file
-    //    }
-    //    Err(e) => {
-    //        if e.kind() == std::io::ErrorKind::PermissionDenied {
-    //            eprintln!("Please run again with appropriate permissions.");
-    //            panic!("{}", e)
-    //        }
-    //        panic!("{}", e);
-    //    }
-    //}
+
+    match std::fs::read(Path::new(&filepath).as_os_str()) {
+       Ok(bytes) => {
+           eprintln!("Length {}", bytes.len().to_string()) // For testing, just print the length of the file
+       }
+       Err(e) => {
+           if e.kind() == std::io::ErrorKind::NotFound {
+               eprintln!("Please check path {}", &filepath);
+               panic!("{}", e)
+           }
+           if e.kind() == std::io::ErrorKind::PermissionDenied {
+               eprintln!("Please run again with appropriate permissions.");
+               panic!("{}", e)
+           }
+           panic!("{}", e);
+       }
+    }
 
     return Rinex::default();
 }
 
 fn write_rnx_file(rinexrec: Rinex) {
-    rinexrec.to_file("").expect("TODO: panic message");
+    //rinexrec.to_file("").expect("TODO: panic message");
 }
