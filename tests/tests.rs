d446eaf5b8fbaa317423a3db9feba0111b6ebce9
Lars Næsbye Christensen <lars@naesbye.dk>
2024-01-13 23:04:38 +0100

    require newer Rust version

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,7 +1,7 @@
 //! Here we define unit tests and other tests to be run
 #[cfg(test)]
 mod tests {
-    #[test]
+    #[test] // placeholder
     fn it_works() {
         let result = 2 + 2;
         assert_eq!(result, 4);
